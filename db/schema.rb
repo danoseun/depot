d64c442c0ea9cd42b8cbd2f24d26500f22299cd4
danoseun <ayodejisomefun@yahoo.com>
2023-11-08 23:08:08 +0100

    add more validations and fine tune tests

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_210729) do
     t.string "title"
     t.text "description"
     t.string "image_url"
-    t.decimal "price", precision: 8
+    t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
